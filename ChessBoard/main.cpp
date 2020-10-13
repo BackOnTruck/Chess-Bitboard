bf65d75a9e69da36958be1f1445e3fb64fab4a6d
BackOnTruck <36351601+BackOnTruck@users.noreply.github.com>
2020-10-13 11:31:52 +0800

    Update main.cpp

--- ChessBoard/main.cpp
+++ ChessBoard/main.cpp
@@ -89,18 +89,3 @@ int main()
 		}
 	}
 }
-hite - Uppercase, Black - Lowercase, # - Number of consecutive empty square\n\t\
-e.g. 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'";
-
-int main()
-{
-	for(string s;;)
-	{
-		printf("%s\n\nFen (Enter to get default, Ctrl+D to quit) >>> ",fenf);
-		if(!getline(cin,s)){puts("\n\nQuitting...");break;}
-		if(s.empty())s="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-		board b(s);
-		for(int i=1;i<6;i++)b.ss={},printf("Perft %d = %10d,",i,b.perft(i)),printf(" %9d captures, %6d e.p's, %7d castles, %8d promotions, %8d checks.\n",b.ss.capt,b.ss.enp,b.ss.castle,b.ss.prom,b.ss.check);
-	}
-}
